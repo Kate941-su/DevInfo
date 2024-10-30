34ada2a5416700951025be8fdb3c5f55bb98b3f4
Kaito Kitaya <kaito.kitaya.deutschland@gmail.com>
2024-10-30 14:25:25 +0100

    Initial Commit

---
+++ DeviceInfo/DeviceInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceInfoApp.swift
+//  DeviceInfo
+//
+//  Created by Kaito Kitaya on 30.10.24.
+//
+
+import SwiftUI
+
+@main
+struct DeviceInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeviceInfoTests/DeviceInfoTests.swift
@@ -0,0 +1,16 @@
+//
+//  DeviceInfoTests.swift
+//  DeviceInfoTests
+//
+//  Created by Kaito Kitaya on 30.10.24.
+//
+
+import Testing
+
+struct DeviceInfoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
